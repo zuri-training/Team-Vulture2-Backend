0cb989ac39727fc60c515b90593807ebe0a65a44
Tolulope Amole <amoletolulope@gmail.com>
2022-12-07 08:15:23 +0100

    fbase files commit

---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const {config} = require("dotenv")
+
+config()
+
+const connectionString = process.env.MONGO_DB_LOCAL
+const connect = async ()=>{
+    try {
+        mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("database connection successful")
+    } catch (error) {
+        console.log(err)
+    }
+}
+
+
+module.exports = connect
