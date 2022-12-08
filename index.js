a5c2d722a0c908736e4cf3b641f3a9d1ecfad108
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2022-12-08 07:38:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+app.use("/", (req, res) => {
+    res.send(`Hello Team-Vulture2`);
+})
+
+app.listen(PORT, () => {
+    console.log(`Our App is running at port ${PORT}`);
+})
