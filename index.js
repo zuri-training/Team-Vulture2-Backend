4510ed942cf39d01e0f471fc3b37e583213ed53b
Tolulope Amole <amoletolulope@gmail.com>
2022-12-09 14:44:41 +0100

    vercel file added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const connect = require("./src/config/database")
 const route = require("./src/router/route")
 connect()
 config()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 app.use("/", route)
@@ -12,3 +12,9 @@ app.use("/", route)
 app.listen(port, function(){
     console.log(`app listening on port ${port}`)
 })
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+module.exports = app
