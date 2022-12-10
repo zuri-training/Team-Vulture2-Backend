b5709baea9ce22b03a602c1cf9179a65c432c6e5
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2022-12-10 09:15:57 +0100

    add

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username: {
         type:String,
         required:true
-    }
+    },
     email: {
         type:String,
         required:true,
@@ -13,17 +13,17 @@ const userSchema = new mongoose.Schema({
         type:String,
         required: true
     },
-    
+
     lastName: {
         type:String,
         required:true
     },
-    
+
     phoneNumber: {
         type:String,
         required:true
     },
-    
+
     password: {
         type:String ,
         required:true
