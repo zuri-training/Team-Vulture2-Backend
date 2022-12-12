d44216773745e051c504724294bbb21a48517999
Tolulope Amole <amoletolulope@gmail.com>
2022-12-12 10:48:33 +0100

    API documentation link attahed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use("/", route)
 app.get("/", (req, res)=>{
-    res.send("Vulterm App API")
+    res.send("Vulterm App API\nCheck documentation on how to use this API on https://github.com/Zuri-training/Team-Vulture2-Backend#readme")
 })
 app.listen(port, function(){
     console.log(`app listening on port ${port}`)
