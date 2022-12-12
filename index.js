fb37cd1e9b1ae716def399f9090b342d532fa30a
Tolulope Amole <amoletolulope@gmail.com>
2022-12-12 11:13:56 +0100

    new html documentation file created

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const fs = require("fs")
+const path = require("path")
 const {config} = require("dotenv")
 const connect = require("./src/config/database")
 const route = require("./src/router/route")
@@ -9,7 +11,12 @@ const app = express()
 app.use(express.json())
 app.use("/", route)
 app.get("/", (req, res)=>{
-    res.send("Vulterm App API\nCheck documentation on how to use this API on https://github.com/Zuri-training/Team-Vulture2-Backend#readme")
+    const filePath = path.join(__dirname, "src", "documentation", "index.html")
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/html")
+    fs.readFile(filePath, "utf8", (err, data)=>{
+        res.end(data)
+    })
 })
 app.listen(port, function(){
     console.log(`app listening on port ${port}`)
