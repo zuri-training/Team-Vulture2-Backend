1f8fa33b753a548dc23f66a92d7fd58e4e3414e2
Tolulope Amole <amoletolulope@gmail.com>
2022-12-14 11:50:27 +0100

    cors origin set to all

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-    next(); 
-})
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use("/", route)
 app.get("/", (req, res)=>{
     const filePath = path.join(__dirname, "src", "documentation", "index.html")
