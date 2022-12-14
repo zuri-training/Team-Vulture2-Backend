22df510f68781af0d920d169dfe703f9037241d5
Tolulope Amole <amoletolulope@gmail.com>
2022-12-14 11:45:36 +0100

    cors header and methods set

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
 app.use("/", route)
 app.get("/", (req, res)=>{
     const filePath = path.join(__dirname, "src", "documentation", "index.html")
